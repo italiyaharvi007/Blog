2800cd8c89aebd13bef17e44bda5136e991eef88
italiyaharvi007 <97499704+italiyaharvi007@users.noreply.github.com>
2025-03-29 13:00:28 +0530

    add : image upload code

--- 28-12-2024/src/controller/UserController.js
+++ 28-12-2024/src/controller/UserController.js
@@ -1,6 +1,39 @@
 const User = require("../model/UserModel")
 const bcrypt = require("bcryptjs")
 const jwt = require('jsonwebtoken');
+const catchAsync = require("../../src/utils/CatchSync");
+const multer = require("multer");
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+const multerStorage = multer.memoryStorage();
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith("image")) {
+        cb(null, true);
+    } else {
+        cb(new AppError("Please upload only images.", 400), false);
+    }
+};
+
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter,
+});
+exports.uploadImage = upload.single("image");
+
+exports.resizeImage = catchAsync(async (req, res, next) => {
+    if (!req.file) return next();
+    const dir = path.join(__dirname, '../../src/public/img/user_image');
+    // Check if the directory exists, if not, create it
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+    const mimetype = req.file.mimetype;
+    const imageType = mimetype.split("/")[1];
+    req.file.image = `userImage-${Date.now()}.${imageType}`;
+    await sharp(req.file.buffer).toFile(`${dir}/${req.file.image}`);
+    next();
+});
 
 exports.SignUp = async (req, res, next) => {
     try {
@@ -67,5 +100,76 @@ exports.Login = async (req, res, next) => {
     } catch (error) {
         console.log(error);
         next(error)
+    }
+}
+
+exports.createUserWithImage = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await User.findOne({ email })
+        if (user) {
+            res.status(400).send({
+                status: "false",
+                message: "Email already exists"
+            })
+        }
+
+        const hashPassword = await bcrypt.hash(password, 12)
+
+        await User.create({
+            name,
+            email,
+            password: hashPassword,
+            image: req.file ? `public/img/user_image/${req.file.image}` : null,
+        })
+
+        res.status(201).send({
+            status: "true",
+            message: "User registered successfully"
+        })
+
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+}
+
+
+exports.updateProfile = async (req, res, next) => {
+    try {
+        const { name, email } = req.body;
+        const findUser = await User.findById(req.params.id);
+
+        const usedEmail = await User.findOne({ email: email, _id: {$ne: findUser} });
+        if (usedEmail) {
+            return res.status(422).send({
+                status: "false",
+                message: "User Email Already Register"
+            })
+        }
+
+        if (findUser.image && req.file) {
+            fs.unlink(path.resolve(`${__dirname}/../../../${findUser.image}`), (err) => {
+                if (err) console.log(err);
+            });
+        }
+
+        if (req.file && req.file.image) {
+            updatedImage = "public/img/user_image/" + req.file.image;
+        } else {
+            updatedImage = findUser.image;
+        }
+
+        await findUser.updateOne({
+            name: name,
+            email: email,
+            image: updatedImage
+        })
+        res.status(200).send({
+            status: "true",
+            message: "User Profile Update successfully"
+        })
+    } catch (error) {
+        console.log(error);
     }
 }
--- 28-12-2024/src/route/UserRoute.js
+++ 28-12-2024/src/route/UserRoute.js
@@ -4,7 +4,15 @@ const validator = require("../middleware/Validator")
 const UserValidation = require("../validation/UserValidation")
 const UserController = require("../controller/UserController")
 
-router.post('/signUp', validator(UserValidation.userSignupSchema), UserController.SignUp)
-router.post('/login', UserController.Login)
+router.post('/signUp', validator(UserValidation.userSignupSchema), UserController.SignUp);
+router.post('/login', UserController.Login);
+router.post('/createUser',
+    UserController.uploadImage,
+    UserController.resizeImage,
+    UserController.createUserWithImage);
+router.patch('/updateUser/:id',
+    UserController.uploadImage,
+    UserController.resizeImage,
+    UserController.updateProfile);
 
 module.exports = router;
